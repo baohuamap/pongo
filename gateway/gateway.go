13d880ae8fe99715d8348e2d7643ac324dbfdc38
bhmap <baohua3110@gmail.com>
2025-07-15 00:31:32 +0700

    add gw

---
+++ gateway/gateway.go
@@ -0,0 +1,91 @@
+package gateway
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"io/fs"
+	"mime"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/grpclog"
+	"google.golang.org/protobuf/proto"
+
+	// gamev1 "github.com/baohuamap/pongo/proto/game/v1"
+	"github.com/baohuamap/pongo/third_party"
+)
+
+func getOpenAPIHandler() http.Handler {
+	mime.AddExtensionType(".svg", "image/svg+xml")
+	// Use subdirectory in embedded files
+	subFS, err := fs.Sub(third_party.OpenAPI, "OpenAPI")
+	if err != nil {
+		panic("couldn't create sub filesystem: " + err.Error())
+	}
+	return http.FileServer(http.FS(subFS))
+}
+
+// Run runs the gRPC-Gateway, dialling the provided address.
+func Run(ctx context.Context, dialAddr string) error {
+	// Adds gRPC internal logs. This is quite verbose, so adjust as desired!
+	log := grpclog.NewLoggerV2(os.Stdout, io.Discard, io.Discard)
+	grpclog.SetLoggerV2(log)
+
+	// Create a client connection to the gRPC Server we just started.
+	// This is where the gRPC-Gateway proxies the requests.
+	conn, err := grpc.NewClient(
+		dialAddr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		return fmt.Errorf("failed to dial server: %w", err)
+	}
+
+	go func() {
+		<-ctx.Done()
+		if err := conn.Close(); err != nil {
+			grpclog.Errorf("Failed to close a client connection to the gRPC server: %v", err)
+		}
+	}()
+
+	gwmux := runtime.NewServeMux(
+		runtime.WithErrorHandler(func(ctx context.Context, mux *runtime.ServeMux, marshaler runtime.Marshaler, w http.ResponseWriter, req *http.Request, err error) {
+			// Custom error handling can be added here
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}),
+		runtime.WithForwardResponseOption(func(ctx context.Context, w http.ResponseWriter, resp proto.Message) error {
+			// Custom response handling can be added here
+			return nil
+		}),
+	)
+
+	oa := getOpenAPIHandler()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "11000"
+	}
+	gatewayAddr := "0.0.0.0:" + port
+	gwServer := &http.Server{
+		Addr: gatewayAddr,
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if strings.HasPrefix(r.URL.Path, "/api") {
+				gwmux.ServeHTTP(w, r)
+				return
+			}
+			oa.ServeHTTP(w, r)
+		}),
+	}
+	// Empty parameters mean use the TLS Config specified with the server.
+	if strings.ToLower(os.Getenv("SERVE_HTTP")) == "true" {
+		log.Info("Serving gRPC-Gateway and OpenAPI Documentation on http://", gatewayAddr)
+		return fmt.Errorf("serving gRPC-Gateway server: %w", gwServer.ListenAndServe())
+	}
+	log.Info("Serving gRPC-Gateway and OpenAPI Documentation on https://", gatewayAddr)
+	return fmt.Errorf("serving gRPC-Gateway server: %w", gwServer.ListenAndServeTLS("", ""))
+}
